062239c88b91833e448c8c62398c1338beb3bfb2
Suzy Veloper <sleitao@uporto.pt>
2019-12-09 11:04:46 +0000

    Correção para que sejam interpretadas as tags html no texto dos sumários

--- view.php
+++ view.php
@@ -240,7 +240,7 @@ echo $OUTPUT->heading($summaries->name);
 				<tr>
 					<td align="justify">
 						<div id="summaries" class="box generalbox generalboxcontent boxaligncenter">
-							<?php echo format_text(stripslashes($r->texto)); ?>
+							<?php echo format_text(stripslashes(html_entity_decode($r->texto))); ?>
 						</div>
 					</td>
 				</tr>

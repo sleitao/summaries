a9094f9f4db293d86cb40a1474a5aeafadaca880
Susana Leitão <sleitao@uporto.pt>
2020-09-17 11:37:21 +0100

    Reintroduzida a validação de IP de origem do pedido preparada para proxy

--- sig_lib.php
+++ sig_lib.php
@@ -113,7 +113,13 @@ function is_utf8($string) {
   
 }
 function get_client_ip() {
-	$ip = $_SERVER['REMOTE_ADDR'];
+    if ($CFG->reverseproxy) {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    else {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        $ip = trim(end(explode(", ",$_SERVER['HTTP_X_FORWARDED_FOR'])));
+    }
     return $ip;
 }
 ?>
